2abbc141a884f64ea95bedc5fd9ce21aaaeb7038
evaniewares <chisopsyelera@gmail.com>
2024-02-11 14:39:16 +0200

    Create BaseModel from dictionary

--- models/base_model.py
+++ models/base_model.py
@@ -10,11 +10,30 @@ from datetime import datetime
 class BaseModel:
     """A BaseModel class"""
 
-    def __init__(self):
-        """Initializes the class"""
-        self.id = str(uuid.uuid4())
-        self.created_at = datetime.now()
-        self.updated_at = datetime.now()
+    def __init__(self, *args, **kwargs):
+        """
+        Initializes the class
+
+        Args:
+            args (list): A list of arguments
+            kwargs (dict): Key-value pair arguments
+        """
+        for key, value in kwargs.items():
+            if key != "__class__":
+                if key == "created_at":
+                    self.__dict__['created_at'] = datetime.strptime(
+                        kwargs['created_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "updated_at":
+                    self.__dict__['updated_at'] = datetime.strptime(
+                        kwargs['updated_at'], '%Y-%m-%dT%H:%M:%S.%f')
+                elif key == "id":
+                    setattr(self, key, str(value))
+                else:
+                    setattr(self, key, value)
+        if not kwargs:
+            self.id = str(uuid.uuid4())
+            self.created_at = datetime.now()
+            self.updated_at = datetime.now()
 
     def __str__(self):
         """Returns a string representation of the instance"""
